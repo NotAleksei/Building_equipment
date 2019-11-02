4161333d2ad876a8639868e35c57814555770f52
unknown <7Lazev@gmail.com>
2019-11-02 14:00:14 +0300

    Create react project

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class App extends React.Component{
+
+state = {
+
+}
+
+// componentDidMount(){
+    
+// var buildings = new Scorocode.Query("buildings");
+// buildings.find().then((finded) => {
+//   let buildings = finded.result;
+//   console.info(buildings);
+// });
+//     console.log(buildings)
+// }
+
+
+render(){
+  return(
+  <div></div>
+  )
+};
+}
+
+
+export default App;
+
+
+
+
+
---
+++ src/index.js
@@ -0,0 +1,24 @@
+/* eslint no-undef: "off"*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+Scorocode.Init({
+	ApplicationID: "3196b2e873234547ad8b06ed636d3538",
+	JavaScriptKey: "5e85f685a23e44e6abad95accc1dd2ea",
+	MasterKey:     "659d718ff9664f6fafbdb79efc93cb34"
+});		
+var buildings = new Scorocode.Query("buildings");
+buildings.find().then((finded) => {
+	let buildings = finded.result;
+	console.info(buildings);
+});
+
+console.log(buildings)
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
